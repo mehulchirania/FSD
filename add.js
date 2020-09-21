03a74078d16f1f4004e14c1be3f2e0593ca9c178
mehulchirania <mehulchirania@gmail.com>
2020-09-21 12:30:53 +0530

    Working code for hands-on

--- add.js
+++ add.js
@@ -1,6 +1,5 @@
-function add(a, b) {
-    var sum = a + b;
-    console.log(sum);
-}
-
-add(1, 2);
+const process = require('process');
+let sum = 0;
+var args = process.argv.slice(2);
+sum = Number(args[1]) + Number(args[0]);
+console.log(sum);
