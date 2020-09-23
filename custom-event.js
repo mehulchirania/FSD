d772c8ea9da2d603a86b35e10b7665335b3edb10
mehulchirania <mehulchirania@gmail.com>
2020-09-23 18:29:49 +0530

    custom event

---
+++ custom-event.js
@@ -0,0 +1,8 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+var Myfunc = function () {
+    console.log('HI THERE ! HAPPY LEARNING');
+}
+eventEmitter.on('MyEvent', Myfunc);
+eventEmitter.emit('MyEvent');
